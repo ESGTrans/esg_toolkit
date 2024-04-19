b473d10c648c4093dcb68ef05c0fe8ced6981945
A-baoYang <rubyyang0615@gmail.com>
2024-04-19 11:38:51 +0000

    Update emvbedding method

--- esg_toolkit/curation_utils.py
+++ esg_toolkit/curation_utils.py
@@ -1,8 +1,7 @@
 from sentence_transformers import SentenceTransformer
 
 
-def to_embeddings(text_list: list):
-    embedding_model_name = "shibing624/text2vec-base-chinese"
+def to_embeddings(text_list: list, embedding_model_name: str = "shibing624/text2vec-base-chinese"):
     embedding_model = SentenceTransformer(embedding_model_name, device="cpu")
     embeddings = embedding_model.encode(text_list)
     print(embeddings.shape)
